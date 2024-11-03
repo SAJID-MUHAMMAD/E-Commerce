416dc38f86d518cac31fcfcefaacff52a3b1ea78
SAJID 23.0 <mdsajid1375@gmail.com>
2024-11-03 23:29:55 +0600

    part-2

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  
+} from "react-router-dom";
+import './App.css'
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home/>} />
+      </Route>
+    )
+  );
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { TbUserExclamation } from "react-icons/tb";
+import { CiSearch } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+const Navbar = () => {
+  return (
+    <nav className="py-7 px-7">
+      <div className="container flex justify-between items-center">
+        <div>
+          <img src="/Logo.png" alt="Logo.png" />
+        </div>
+        <ul className="flex items-center gap-[75px] font-Poppins  text-lg font-medium text-black">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Shop</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+        </ul>
+
+        <div>
+          <ul className="flex items-center gap-11 text-xl">
+            <li>
+              <Link to="">
+                <TbUserExclamation />
+              </Link>
+            </li>
+            <li>
+              <Link to="">
+                <CiSearch />
+              </Link>
+            </li>
+            <li>
+              <Link to="">
+                <CiHeart />
+              </Link>
+            </li>
+            <li>
+              <Link to="">
+                <MdOutlineShoppingCart />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
