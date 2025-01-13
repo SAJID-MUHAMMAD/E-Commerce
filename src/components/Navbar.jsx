28c049fe434b23d45bd5756add8553a074dfb3f5
SAJID 23.0 <mdsajid1375@gmail.com>
2025-01-13 19:43:05 +0600

    part-9

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { TbUserExclamation } from "react-icons/tb";
 import { CiSearch } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { CiMenuFries } from "react-icons/ci";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
   return (
-    <nav className="py-7">
-      <div className="container max-w-screen-xxl xxl:px-24 flex justify-between items-center">
-        <Link to="/">
-          <img src="/Logo.png" alt="Logo.png" />
-        </Link>
-        <ul className="flex items-center gap-[75px] font-Poppins  text-lg font-medium text-black">
+    <nav className="py-7 relative">
+      <div className="container  max-w-screen-xxl xxl:px-24 flex justify-between items-center">
+        <div>
+          <Link to="/">
+            <img src="/Logo.png" alt="Logo.png" />
+          </Link>
+        </div>
+        <ul
+          className={`w-1/2 lg:w-auto flex flex-col ${
+            show ? "-translate-x-full" : "translate-x-0"
+          } transition-all lg:flex-row items-center gap-[75px] font-Poppins  text-lg font-medium text-black absolute lg:static top-full left-0 ${
+            show ? "" : "bg-white"
+          } h-screen lg:h-auto `}
+        >
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -51,6 +61,9 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+        <button className="block lg:hidden" onClick={() => setShow(!show)}>
+          <CiMenuFries />
+        </button>
       </div>
     </nav>
   );
