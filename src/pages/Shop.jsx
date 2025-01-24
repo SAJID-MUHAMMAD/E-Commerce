603d0c42053bc128d6bc71c8434cd71a3c559a9b
SAJID 23.0 <mdsajid1375@gmail.com>
2025-01-24 23:35:13 +0600

    part-10

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Banner from "../components/shop-page/Banner";
+import Filter from "../components/shop-page/Filter";
+import Products from "../components/shop-page/Products";
 
 const Shop = () => {
   return (
     <>
       <Banner />
+      <Filter />
+      <Products itemsPerPage={16} />
     </>
   );
 };
